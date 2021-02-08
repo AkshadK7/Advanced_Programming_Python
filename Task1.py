b3404fa7234a3cfb8c8b21609daa7e83de247639
Akshad Kolhatkar <52818443+AkshadK7@users.noreply.github.com>
2021-02-08 12:53:56 +0530

    Commited Task 1

---
+++ Task1.py
@@ -0,0 +1,19 @@
+"""
+Q] Using a for loop, print a table of powers of x, where x ranges from 1 to 10. For each
+value x, print the quantity x, x 2 , and x 3 . Using tab characters in your print statement
+to make the values line up nicely.
+"""
+
+# Program :
+
+# To take input from the user
+num = int(input("Display Multiplication Table for: "))
+
+# Iterate 10 times from i = 1 to 10
+for i in range(1, 11):
+   print(num, 'x', i, '=', num*i)
+   
+   
+   
+
+# Copyright © 2020 by Akshad Kolhatkar
