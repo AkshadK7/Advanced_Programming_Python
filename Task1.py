2c58e7c1b616c6ebd92828ea57a30384877e76d6
Akshad Kolhatkar <52818443+AkshadK7@users.noreply.github.com>
2021-02-08 13:31:30 +0530

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,18 +1,17 @@
 """
-Q] Using a for loop, print a table of multiples of x, where x ranges from 1 to 10. For each
-value x, print the quantity x, x*2 , and x*3 . Using tab characters in your print statement
+Q] Using a for loop, print a table of powers of x, where x ranges from 1 to 10. For each
+value x, print the quantity x, x^2 and x^3 . Using tab characters in your print statement
 to make the values line up nicely.
 """
 
 # Program :
 
-# To take input from the user
-num = int(input("Display Multiplication Table for: "))
-
-# Iterate 10 times from i = 1 to 10
-for i in range(1, 11):
-   print(num, 'x', i, '=', num*i)
-   
+# Iterate 10 times from x = 1 to 10
+for x in range(1, 11):
+   print( "For value of x : ", x) 
+   print(x, '^', 2, '=', x**2)
+   print(x, '^', 3, '=', x**3)
+   print("\t")
    
    
 
